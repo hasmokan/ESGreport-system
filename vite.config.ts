9680d6fb6936ba50329eed01a3ca5a1309ffdab0
雄恺 陈 <1021056159@qq.com>
2023-04-09 23:38:31 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import alias from "@rollup/plugin-alias";
 import image from '@rollup/plugin-image';
 import externalGlobals from 'rollup-plugin-external-globals';
 // https://vitejs.dev/config/
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 export default defineConfig({
   plugins: [image(),alias(),vue()],
   resolve: {
@@ -22,16 +25,21 @@ export default defineConfig({
     }
   },
   build: {
-    chunkSizeWarningLimit:250000,
     rollupOptions: {
-      external: [],
-      plugins:[
-        externalGlobals({
-
-        })
-      ]
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : "";
+          // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+          // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        },
+      },
     },
-    // ...其他构建配置
   },
   base:'./',
 })
